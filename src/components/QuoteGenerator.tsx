6d1b4b44dfe8c233c347887680148659b5cf3a2d
Paula Mazo <paulamazoster@gmail.com>
2025-07-14 18:52:47 -0500

    feat: add quote generator component with no logic

--- src/components/QuoteGenerator.tsx
+++ src/components/QuoteGenerator.tsx
@@ -1,5 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
 export const QuoteGenerator = () => {
   return (
-    <>Quote Generator</>
+    <Box
+      display="flex"
+      flexDirection="column"
+      textAlign="center"
+      position="sticky"
+      mb={4}
+    >
+      <Typography variant="body1" sx={{ fontStyle: 'italic' }}>
+        “No pain, no gain.”
+      </Typography>
+    </Box>
   )
 };
