f23cbadc8e25ff5dc7bbe9450290739d760647f7
Paula Mazo <paulamazoster@gmail.com>
2025-07-14 18:53:37 -0500

    feat: add exercise form with no logic

--- src/components/ExerciseForm.tsx
+++ src/components/ExerciseForm.tsx
@@ -1,5 +1,44 @@
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+
 export const ExerciseForm = () => {
+  const [name, setName] = useState("");
+  const [sets, setSets] = useState(0);
   return (
-    <>Exercise Form</>
+    <Box
+      component="form"
+      display="flex"
+      alignItems="flex-start"
+      position="sticky"
+      py={1}
+      px={2}
+      gap={2}
+      mb={4}
+      zIndex={2}
+    >
+      <TextField
+        label="Exercise name"
+        value={name}
+        helperText="E.g.: Thrusters"
+        onChange={(e) => setName(e.target.value)}
+        size="small"
+        fullWidth
+        required
+      />
+      <TextField
+        label="Sets"
+        value={sets}
+        onChange={(e) => setSets(Number(e.target.value))}
+        size="small"
+        fullWidth
+        required
+      />
+      <Button
+        type="submit"
+        variant="contained"
+      >
+        Add
+      </Button>
+    </Box>
   )
 };
