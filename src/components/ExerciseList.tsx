e9c0ce84317a9b12dd8b152a30d03f5261b41a94
Paula Mazo <paulamazoster@gmail.com>
2025-07-15 09:44:46 -0500

    feat: display a message when exercise list is empty

--- src/components/ExerciseList.tsx
+++ src/components/ExerciseList.tsx
@@ -1,8 +1,9 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { ExerciseCard } from "./ExerciseCard";
 import type { ExerciseListProps } from "@/types";
 
 export const ExerciseList = ({ exercises }: ExerciseListProps) => {
+  if (exercises.length === 0) return <Typography variant="h6">No exercises added yet.</Typography>;
   return (
     <Box flex={1} overflow="auto" px={2} py={1}>
       <Stack spacing={2}>
