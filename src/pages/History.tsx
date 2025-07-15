1776820495777664c19b2988d3ffd7e9f8dc3189
Paula Mazo <paulamazoster@gmail.com>
2025-07-15 10:13:01 -0500

    feat: create history page basic layout

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,7 +1,29 @@
+import { useExercise } from "@/providers/Exercise";
+import { Container, List, ListItem, ListItemText, Typography } from "@mui/material";
+
 export const History = () => {
+    const { exercises } = useExercise();
+    const completedExercises = exercises.filter((exercise) => exercise.isDone);
     return (
-        <>
-            History page
-        </>
+        <Container maxWidth="md" sx={{ pt: 4 }}>
+            <Typography variant="h4" mb={4}>
+                History
+            </Typography>
+            {completedExercises.length === 0 ? (
+                <Typography variant="h6">No completed exercises yet.</Typography>
+            ) : (
+                <List>
+                    {completedExercises.map((exercise) => (
+                        <ListItem key={exercise.id}>
+                            <ListItemText
+                                primary={exercise.name}
+                                secondary={`Sets: ${exercise.completedSets} [COMPLETED]`}
+                            />
+                        </ListItem>
+                    ))}
+                </List>                   
+            )
+            }
+        </Container>
     );
 };
