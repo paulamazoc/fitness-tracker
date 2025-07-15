7be3adb8cefadfbd41ef45073e76c2b5802b7d49
Paula Mazo <paulamazoster@gmail.com>
2025-07-15 09:49:28 -0500

    test: refactor common elements definition

--- src/components/ExerciseForm.spec.tsx
+++ src/components/ExerciseForm.spec.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, afterEach } from 'vitest';
+import { describe, it, expect, vi, afterEach, beforeEach } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { ExerciseForm } from './ExerciseForm';
 
@@ -10,23 +10,26 @@ vi.mock("@/providers/Exercise", () => ({
 }));
 
 describe('ExerciseForm', () => {
+
+  let nameInput: HTMLInputElement;
+  let setsInput: HTMLInputElement;
+
+  beforeEach(() => {
+    render(<ExerciseForm />);
+    nameInput = screen.getByLabelText(/exercise name/i);
+    setsInput = screen.getByLabelText(/sets/i);
+  });
+
   afterEach(() => {
     vi.clearAllMocks();
   });
 
   it("renders the form with initial values", () => {
-    render(<ExerciseForm />);
-
-    expect(screen.getByLabelText(/exercise name/i)).toHaveValue("");
-    expect(screen.getByLabelText(/sets/i)).toHaveValue(1);
+    expect(nameInput).toHaveValue("");
+    expect(setsInput).toHaveValue(1);
   });
 
   it("does not submit the form if fields are empty", () => {
-    render(<ExerciseForm />);
-
-    const nameInput = screen.getByLabelText(/exercise name/i);
-    const setsInput = screen.getByLabelText(/sets/i);
-
     fireEvent.change(nameInput, { target: { value: " " } });
     fireEvent.change(setsInput, { target: { value: "" } });
     fireEvent.click(screen.getByRole("button", { name: /Add/i }));
@@ -35,11 +38,6 @@ describe('ExerciseForm', () => {
   });
 
   it("does not submit the form if sets is 0", () => {
-    render(<ExerciseForm />);
-
-    const nameInput = screen.getByLabelText(/exercise name/i);
-    const setsInput = screen.getByLabelText(/sets/i);
-
     fireEvent.change(nameInput, { target: { value: "snatch" } });
     fireEvent.change(setsInput, { target: { value: "0" } });
     fireEvent.click(screen.getByRole("button", { name: /Add/i }));
@@ -48,11 +46,6 @@ describe('ExerciseForm', () => {
   });
 
   it("submit the form and reset to default values", () => {
-    render(<ExerciseForm />);
-
-    const nameInput = screen.getByLabelText(/exercise name/i);
-    const setsInput = screen.getByLabelText(/sets/i);
-
     fireEvent.change(nameInput, { target: { value: "pushups" } });
     fireEvent.change(setsInput, { target: { value: "5" } });
     fireEvent.click(screen.getByRole("button", { name: /Add/i }));
@@ -63,10 +56,6 @@ describe('ExerciseForm', () => {
   });
 
   it("updates input values when user types", () => {
-    render(<ExerciseForm />);
-    const nameInput = screen.getByLabelText(/exercise name/i);
-    const setsInput = screen.getByLabelText(/sets/i);
-
     fireEvent.change(nameInput, { target: { value: "box jumps" } });
     fireEvent.change(setsInput, { target: { value: "3" } });
 
