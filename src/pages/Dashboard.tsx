384091dcf5155008716bbabd4eb0d2c351fd3309
Paula Mazo <paulamazoster@gmail.com>
2025-07-15 21:59:18 -0500

    feat: add navigation from dashboard to history

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,24 @@
 import { ExerciseForm } from "@/components/ExerciseForm";
 import { ExerciseList } from "@/components/ExerciseList";
 import { QuoteGenerator } from "@/components/QuoteGenerator";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useExercise } from '@/providers/Exercise'
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
     const { exercises } = useExercise();
+    const navigate = useNavigate();
     return (
         <Box
             display="flex"
             flexDirection="column"
             minHeight="100vh"  
         >
+            <Box position="absolute" top={24} right={16}>
+                <Button variant="text" size="small" onClick={() => navigate('/history')}>
+                    Go to History ⟶
+                </Button>
+            </Box>
             <QuoteGenerator />
             <ExerciseForm />
             <Typography variant="h4" mb={4}>
